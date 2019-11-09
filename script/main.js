765e2d963a6917f6b8784da1ec32b51895c7a7fb
TomGrooves <kasperfryd@gmail.com>
2019-11-09 19:26:25 +0100

    updated functions with error check and clearform

--- script/main.js
+++ script/main.js
@@ -16,17 +16,46 @@ fetch(urlAll)
 .then(function(responseAsJson) {
   // Do stuff with the JSON
 
-  let divContainer = document.createElement("ol");
+  let divContainer = document.createElement("div");
+  divContainer.classList.add("maingrid");
   
+  let grid1 = document.createElement("div");
+  let grid2 = document.createElement("div");
+  let grid3 = document.createElement("div");
+  let grid4 = document.createElement("div");
+
   for (data in responseAsJson){
     let list = document.createElement("li");
-    list.append("ID: " + responseAsJson[data].id + " - ");
-    list.append("Username: " +responseAsJson[data].username + " - ");
-    list.append("Email: " +responseAsJson[data].email + " - ");
-    list.append("Password: " +responseAsJson[data].passwrd + " - ");
-    divContainer.append(list);
+    list.classList.add("griditem");
+    list.append("ID: " + responseAsJson[data].id + " ");
+    grid1.append(list);
   }
-  mainContainer.append(divContainer);
+
+  for (data in responseAsJson){
+    let list = document.createElement("li");
+    list.classList.add("griditem");
+    list.append("Username: " +responseAsJson[data].username + " ");
+    grid2.append(list);
+  }
+
+  for (data in responseAsJson){
+    let list = document.createElement("li");
+    list.classList.add("griditem");
+    list.append("Email: " +responseAsJson[data].email+ " ");
+    grid3.append(list);
+  }  
+
+  for (data in responseAsJson){
+    let list = document.createElement("li");
+    list.classList.add("griditem");
+    list.append("Password: " +responseAsJson[data].passwrd + " ");
+    grid4.append(list);
+  }
+
+  mainContainer.append(grid1);
+  mainContainer.append(grid2);
+  mainContainer.append(grid3);
+  mainContainer.append(grid4);
   
 })
 .catch(function(error) {
@@ -37,11 +66,12 @@ fetch(urlAll)
 /** Fetch a single user with ID and display it */
 function fetchSingleUser(){
   clearAll();
-  //mainContainer = "";
-  let dataID = 0;
+  let dataID = "";
   dataID = document.getElementById("idfield").value;
-  console.log(dataID);
   let urlSingle = 'http://localhost/rest-api/entry.php/?ID='+dataID;
+  if (!dataID == ""){
+   
+  
   fetch(urlSingle)
   .then (function(response) {
     if (!response.ok) {
@@ -52,30 +82,67 @@ function fetchSingleUser(){
   })
   .then(function(responseAsJson) {
     // Do stuff with the JSON
-    let divContainer = document.createElement("ol");
+    let divContainer = document.createElement("div");
+    divContainer.classList.add("maingrid");
+    
+    let grid1 = document.createElement("div");
+    let grid2 = document.createElement("div");
+    let grid3 = document.createElement("div");
+    let grid4 = document.createElement("div");
   
-    console.log(responseAsJson);
     for (data in responseAsJson){
       let list = document.createElement("li");
-      list.append("ID: " + responseAsJson[data].id + " - ");
-      list.append("Username: " + responseAsJson[data].username + " - ");
-      list.append("Email: " + responseAsJson[data].email + " - ");
-      list.append("Password: " + responseAsJson[data].passwrd + " - ");
-      divContainer.append(list);
+      list.classList.add("griditem");
+      list.append("ID: " + responseAsJson[data].id + " ");
+      grid1.append(list);
     }
-    mainContainer.append(divContainer);
+  
+    for (data in responseAsJson){
+      let list = document.createElement("li");
+      list.classList.add("griditem");
+      list.append("Username: " +responseAsJson[data].username + " ");
+      grid2.append(list);
+    }
+  
+    for (data in responseAsJson){
+      let list = document.createElement("li");
+      list.classList.add("griditem");
+      list.append("Email: " +responseAsJson[data].email+ " ");
+      grid3.append(list);
+    }  
+  
+    for (data in responseAsJson){
+      let list = document.createElement("li");
+      list.classList.add("griditem");
+      list.append("Password: " +responseAsJson[data].passwrd + " ");
+      grid4.append(list);
+    }
+  
+    mainContainer.append(grid1);
+    mainContainer.append(grid2);
+    mainContainer.append(grid3);
+    mainContainer.append(grid4);
+    
   
 })
 .catch(function(error) {
   console.log('Looks like there was a problem: \n', error);
 });
+  }
+  else{
+    let divContainer = document.createElement("div");
+    divContainer.innerHTML = '<h2>You need to enter a valid id</h2>';
+    mainContainer.append(divContainer);
+  }
 }
 
 /** Update a user with a certain id */
 function updateUser(confirm){
- 
+  clearAll(); 
+  let check = checkInputs();
+  if (check == ""){
   if (confirm){
-  clearAll();
+
   //fetchSingleUser();
 
   dataID = document.getElementById("id").value;
@@ -109,6 +176,12 @@ fetch(urlSingle, {
     confirmBox.innerHTML = '<h2>Are you sure you want to update user?</h2><br><input type="button" value="Update" onclick="confirmUpdate()"><input type="button" value="Cancel" onclick="deny()">'
     mainContainer.append(confirmBox); 
   }
+  }
+  else{
+    let confirmBox = document.createElement("h2");
+    confirmBox.innerHTML = check;
+    mainContainer.append(confirmBox);
+  }
 }
 
 let confirmed = false;
@@ -116,6 +189,7 @@ let confirmed = false;
 /** Delete a user with a certain id */
 function deleteUser(confirm){
   console.log(confirm);
+
   if (confirm){
     clearAll();
     dataID = document.getElementById("idfield").value;
@@ -151,19 +225,22 @@ function confirmDelete(){
 function confirmUpdate(){
   confirmed = true;
   updateUser(confirmed);
+  clearForm();
 }
 
 /** Set boolean confirm to false */
 function deny(){
   confirmed = false;
-  deleteBox.innerHTML = " "
+  clearAll();
 }
 
 /** Create a new user with name, email and password */
 function createUser(){
   clearAll();
 
-  dataID = document.getElementById("id").value;
+  //dataID = document.getElementById("id").value;
+  let check = checkInputs();
+  if (check == ""){
   const data = JSON.stringify ({
 
     "username": document.getElementById("username").value,
@@ -182,10 +259,48 @@ fetch(url, {
 confirmBox = document.createElement("div");
 confirmBox.innerHTML = '<h2>User has been added</h2>'
 mainContainer.append(confirmBox);
-
+clearForm();
+}
+else {
+  confirmBox = document.createElement("h2");
+confirmBox.innerHTML = check;
+mainContainer.append(confirmBox);
+}
 }
 
 /** Clear all data from the view */
 function clearAll(){
   mainContainer.innerHTML = " ";
+  //clearForm();
+}
+
+function toggleMenu(classname){
+  console.log(classname);
+  elem = document.getElementsByClassName(classname);
+  for (el of elem){
+  console.log(el);
+  el.classList.toggle("hidden");
+  }
+}
+
+
+function clearForm(){
+  inputs = document.getElementsByClassName("input");
+  for (i of inputs){
+  i.value = "";
+  }
+}
+
+function checkInputs(){
+  let inputs = document.getElementsByClassName("input");
+  for (let i of inputs){
+  if (i.value == ""){
+    let msg = "Error! You need to fill in all fields";
+    return msg;
+  }
+  else{
+    let msg = "";
+    return msg;
+  }
+  }
 }
